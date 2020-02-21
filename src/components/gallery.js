30b4a8af339f0bc04f1e278ce050733df2e97823
Karlo Mrzlečki <km51049@fer.hr>
2020-02-21 14:38:03 +0100

    dodana lokalizacija, ispravljena responzivnost

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -2,10 +2,34 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import { Controller, Scene } from "react-scrollmagic"
+import translate from "translate-js"
 
 import "./gallery.scss"
 
 function Gallery(props) {
+  //localization
+  function getLocale() {
+    var lang = ""
+    if (typeof navigator !== "undefined") {
+      if (navigator.languages !== undefined) lang = navigator.languages[0]
+      else lang = navigator.language
+
+      if (lang.substring(0, 2).localeCompare("hr") !== 0) return "en"
+      else return "hr"
+    }
+  }
+
+  var locale = getLocale()
+
+  translate.add({ interior: "INTERIJER" }, "hr")
+  translate.add({ interior: "INTERIOR" }, "en")
+
+  translate.add({ exterior: "EKSTERIJER" }, "hr")
+  translate.add({ exterior: "EXTERIOR" }, "en")
+
+  translate.add({ product: "PROIZVODI" }, "hr")
+  translate.add({ product: "PRODUCTS" }, "en")
+
   return (
     <div className="gallery">
       <div className="galleryContainer">
@@ -15,7 +39,7 @@ function Gallery(props) {
               classToggle="text-line"
               triggerElement="#trigger1"
               duration="0"
-              triggerHook="0.8"
+              triggerHook="0.9"
             >
               <svg height="16vw" width="60vw" stroke="#000000" stroke-width="1">
                 <text
@@ -27,7 +51,7 @@ function Gallery(props) {
                   font-size="8vw"
                   visibility="hidden"
                 >
-                  {props.title1}
+                  {translate("interior", null, { locale: locale })}
                 </text>
               </svg>
             </Scene>
@@ -51,15 +75,9 @@ function Gallery(props) {
               classToggle="text-line"
               triggerElement="#trigger2"
               duration="0"
-              triggerHook="0.8"
+              triggerHook="0.9"
             >
-              <svg
-                height="16vw"
-                width="60vw"
-                stroke="#000000"
-                stroke-width="1"
-                class="text-line"
-              >
+              <svg height="16vw" width="60vw" stroke="#000000" stroke-width="1">
                 <text
                   font-family="Metropolis"
                   font-style="black"
@@ -69,7 +87,7 @@ function Gallery(props) {
                   font-size="8vw"
                   visibility="hidden"
                 >
-                  {props.title2}
+                  {translate("exterior", null, { locale: locale })}
                 </text>
               </svg>
             </Scene>
@@ -93,7 +111,7 @@ function Gallery(props) {
               classToggle="text-line"
               triggerElement="#trigger3"
               duration="0"
-              triggerHook="0.8"
+              triggerHook="0.9"
             >
               <svg height="16vw" width="60vw" stroke="#000000" stroke-width="1">
                 <text
@@ -105,7 +123,7 @@ function Gallery(props) {
                   font-size="8vw"
                   visibility="hidden"
                 >
-                  {props.title3}
+                  {translate("product", null, { locale: locale })}
                 </text>
               </svg>
             </Scene>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react"
-import Helmet from 'react-helmet'
+import Helmet from "react-helmet"
 import gsap from "gsap"
 
 //import Image from "../components/image"
@@ -12,64 +12,59 @@ import Gallery from "../components/gallery"
 import "../styles/styles.scss"
 
 function IndexPage() {
-
   useEffect(() => {
     //gsap timeline
-    const tl = gsap.timeline();
+    const tl = gsap.timeline()
 
     tl.from(".landing-text", 2, {
-      display: 'none',
+      display: "none",
       opacity: 0,
       y: 50,
       ease: "power4.out",
       skewY: 2,
-      stagger: {amount: 0.3}
-    }).from(".header-menu, .logo", 1.5, {
-      display: 'none',
-      opacity: 0,
-      y: 10,
-      ease: "power4.out",
-      stagger: {amount: 0.3}
-    }).from(".icon-scroll", 4, {
-      display: 'none',
-      opacity: 0,
-      y: -10,
-      ease: "power4.out"
+      stagger: { amount: 0.3 },
     })
-
-}, []);
-
+      .from(".header-menu, .logo", 1.5, {
+        display: "none",
+        opacity: 0,
+        y: 10,
+        ease: "power4.out",
+        stagger: { amount: 0.3 },
+      })
+      .from(".icon-scroll", 4, {
+        display: "none",
+        opacity: 0,
+        y: -10,
+        ease: "power4.out",
+      })
+  }, [])
 
   return (
     <main>
-    <Helmet>
-      <html lang="hr" />
-      <title>ARTERION obrt za dizajn</title>
-      <description>
-        ARTERION obrt za dizajn radi 3D vizualizacije interijera, eksterijera i
-        raznih proizvoda.
-      </description>
-    </Helmet>
+      <Helmet>
+        <html lang="hr" />
+        <title>ARTERION obrt za dizajn</title>
+        <description>
+          ARTERION obrt za dizajn radi 3D vizualizacije interijera, eksterijera
+          i raznih proizvoda.
+        </description>
+      </Helmet>
 
-    <div>
-      <Header />
-      <Landing />
-      <Gallery
-        title1="INTERIJER"
-        imagetop1={require("../images/interior_1.jpg")}
-        imagebottom1={require("../images/interior_2.jpg")}
-        title2="EKSTERIJER"
-        imagetop2={require("../images/exterior_1.jpg")}
-        imagebottom2={require("../images/exterior_2.jpg")}
-        title3="PROIZVODI"
-        imagetop3={require("../images/product_2.jpg")}
-        imagebottom3={require("../images/product_1.jpg")}
-      />
-      <Contact>
-    </div>
-  </main>
-  );
+      <div>
+        <Header />
+        <Landing />
+        <Gallery
+          imagetop1={require("../images/interior_1.jpg")}
+          imagebottom1={require("../images/interior_2.jpg")}
+          imagetop2={require("../images/exterior_1.jpg")}
+          imagebottom2={require("../images/exterior_2.jpg")}
+          imagetop3={require("../images/product_2.jpg")}
+          imagebottom3={require("../images/product_1.jpg")}
+        />
+        <Contact />
+      </div>
+    </main>
+  )
 }
-
 
 export default IndexPage
