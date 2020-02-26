5b9ffd2d9c24f81e5a6cd7cde1f9bcec4d9df43b
Karlo Mrzlečki <km51049@fer.hr>
2020-02-26 20:21:17 +0100

    galerija + kontakt; ima bugova, ali ja sam ih proglasil featurom

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,90 +1,97 @@
 import React, { useState } from "react"
-import Lottie from 'react-lottie'
-import ScrollTrigger from 'react-scroll-trigger';
+// import Lottie from 'react-lottie'
+// import ScrollTrigger from "react-scroll-trigger"
 import translate from "translate-js"
-import * as contactAnimationDataHR from '../images/contact_hr.json'
-import * as contactAnimationDataEN from '../images/contact_en.json'
+// import * as contactAnimationDataHR from '../images/contact_hr.json'
+// import * as contactAnimationDataEN from '../images/contact_en.json'
 
 const Contact = () => {
+  // //Lotie toggle
+  // const [isStopped, setStopped] = useState(true);
 
-    //Lotie toggle
-    const [isStopped, setStopped] = useState(true);
+  //localization
+  function getLocale() {
+    var lang = ""
+    if (typeof navigator !== "undefined") {
+      if (navigator.languages !== undefined) lang = navigator.languages[0]
+      else lang = navigator.language
 
-    //localization
-    function getLocale() {
-        var lang = "";
-        if (typeof navigator !== "undefined"){
-          if (navigator.languages !== undefined) lang = navigator.languages[0]; 
-          else lang = navigator.language;
-    
-          if (lang.substring(0,2).localeCompare("hr") !== 0) return "en"
-          else return "hr"
-        }
+      if (lang.substring(0, 2).localeCompare("hr") !== 0) return "en"
+      else return "hr"
     }
-    
-    var locale = getLocale();
+  }
 
-    translate.add({country: 'Hrvatska', owner: 'vl. Siniša Slovenec'}, 'hr');
-    translate.add({country: 'Croatia', owner: 'owner: Siniša Slovenec'}, 'en');
+  var locale = getLocale()
 
+  translate.add(
+    {
+      country: "Hrvatska",
+      owner: "vl. Siniša Slovenec",
+      title1: "Sviđa Vam se što vidite?",
+      title2: "Slobodno se javite.",
+    },
+    "hr"
+  )
+  translate.add(
+    {
+      country: "Croatia",
+      owner: "owner: Siniša Slovenec",
+      title1: "Like what you see?",
+      title2: "Feel free to contact.",
+    },
+    "en"
+  )
 
-    //scroll trigger
-    const onEnterViewport = () => {
-        setStopped(false);
-    }
+  // //scroll trigger
+  // const onEnterViewport = () => {
+  //     setStopped(false);
+  // }
 
-    const onExitViewport = () => {
-        setStopped(true);
-    }
+  // const onExitViewport = () => {
+  //     setStopped(true);
+  // }
 
+  // //Lottie options
+  // if (typeof locale !=="undefined") {
+  //     var defaultOptions = {
+  //         loop: false,
+  //         autoplay: false,
+  //         animationData:
+  //             locale.localeCompare("hr") === 0
+  //                 ? contactAnimationDataHR.default
+  //                 : contactAnimationDataEN.default,
+  //         rendererSettings: {
+  //         preserveAspectRatio: 'xMidYMid slice'
+  //       }
+  //     }
+  // }
 
-    //Lottie options
-    if (typeof locale !=="undefined") {
-        var defaultOptions = {
-            loop: false,
-            autoplay: false, 
-            animationData:
-                locale.localeCompare("hr") === 0
-                    ? contactAnimationDataHR.default
-                    : contactAnimationDataEN.default,
-            rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice'
-          }
-        }
-    }
-   
-    return (
-        <div className="contact-cont">
-                
-            <div className="contact-bg"></div>
+  return (
+    <div className="contact-cont">
+      <div className="contact-top">
+        <h2>{translate("title1", null, { locale: locale })}</h2>
+        <h1>{translate("title2", null, { locale: locale })}</h1>
+      </div>
 
-            <div className="contact-top">
-                <div className="contact-animation-cont">
-                    <ScrollTrigger onEnter={() => onEnterViewport()} onExit={() => onExitViewport()} />
-                    <Lottie options={defaultOptions} isStopped = {isStopped} isClickToPauseDisabled={true} className="contact-animation" id="contact-animation"
-                    />
-                </div>
-            </div>
-
-            <div className="contact-middle">
-                <div className="contact-left">
-                    <a href="mailto:info@arterion.design">info@arterion.design</a>
-                    <a href="tel:385 123 456">+385 123 456</a>
-                </div>
-
-                <div className="contact-right">
-                    <h3 id="company-title">ARTERION obrt za dizajn</h3>
-                    <h3>{translate('owner', null, {locale: locale})}</h3>
-                    <h3>Jalšje 22, Veliko Trgovišće</h3>
-                    <h3>{translate('country', null, {locale: locale})}</h3>
-                </div>
-            </div>
-
-            <div className="contact-bottom">
-                <a href="https://apeiron.media">Made with &hearts; by apeiron</a>
-            </div>
+      <div className="contact-middle">
+        <div className="contact-left">
+          <a href="mailto:info@arterion.design">info@arterion.design</a>
+          <a href="tel:385 123 456">+385 123 456</a>
         </div>
-    )
+
+        <div className="contact-right">
+          <h3 id="company-title">ARTERION obrt za dizajn</h3>
+          <h3>{translate("owner", null, { locale: locale })}</h3>
+          <h3>Jalšje 22, Veliko Trgovišće</h3>
+          <h3>{translate("country", null, { locale: locale })}</h3>
+        </div>
+      </div>
+
+      <div className="contact-bottom">
+        <a href="https://apeiron.media">Made with &hearts; by apeiron</a>
+      </div>
+    </div>
+  )
 }
 
 export default Contact
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react"
 import Helmet from "react-helmet"
 import gsap from "gsap"
 import translate from "translate-js"
+import { ParallaxProvider } from "react-scroll-parallax"
 
 //import Image from "../components/image"
 //import SEO from "../components/seo"
@@ -13,7 +14,6 @@ import Gallery from "../components/gallery"
 import "../styles/styles.scss"
 
 function IndexPage() {
-
   useEffect(() => {
     //gsap timeline
     const tl = gsap.timeline()
@@ -21,23 +21,23 @@ function IndexPage() {
     tl.set(".landing-text", {
       autoAlpha: 0,
       y: 50,
-      skewY: 2
+      skewY: 2,
     })
-    .set(".header-menu, .logo", {
-      autoAlpha: 0,
-      y: 10
-    })
-    .set(".icon-scroll", {
-      autoAlpha: 0,
-      y: -10
-    })  
-    .to(".landing-text", 2, {
-      autoAlpha: 1,
-      y: 0,
-      ease: "power4.out",
-      skewY: 0,
-      stagger: { amount: 0.3 },
-    })
+      .set(".header-menu, .logo", {
+        autoAlpha: 0,
+        y: 10,
+      })
+      .set(".icon-scroll", {
+        autoAlpha: 0,
+        y: -10,
+      })
+      .to(".landing-text", 2, {
+        autoAlpha: 1,
+        y: 0,
+        ease: "power4.out",
+        skewY: 0,
+        stagger: { amount: 0.3 },
+      })
       .to(".header-menu, .logo", 1.5, {
         autoAlpha: 1,
         y: 0,
@@ -53,40 +53,59 @@ function IndexPage() {
 
   //localization
   function getLocale() {
-    var lang = "";
-    if (typeof navigator !== "undefined"){
-      if (navigator.languages !== undefined) lang = navigator.languages[0]; 
-      else lang = navigator.language;
+    var lang = ""
+    if (typeof navigator !== "undefined") {
+      if (navigator.languages !== undefined) lang = navigator.languages[0]
+      else lang = navigator.language
 
-      if (lang.substring(0,2).localeCompare("hr") !== 0) return "en"
+      if (lang.substring(0, 2).localeCompare("hr") !== 0) return "en"
       else return "hr"
     }
   }
 
-  var locale = getLocale();
+  var locale = getLocale()
 
-  translate.add({metaTitle: 'ARTERION | 3D vizualizacije interijera, eksterijera i proizvoda',
-  metaDesc: 'ARTERION obrt za dizajn pruža usluge izrade realističnih 3D vizualizacija interijera i eksterijera te raznih proizvoda.'}, 'hr');
-  translate.add({metaTitle: 'ARTERION | 3D interior, exterior and product visualization',
-  metaDesc: 'ARTERION offers 3D visualization services, ranging from high-quality interior and exterior renders to 3D product modeling and rendering.'}, 'en');
+  translate.add(
+    {
+      metaTitle:
+        "ARTERION | 3D vizualizacije interijera, eksterijera i proizvoda",
+      metaDesc:
+        "ARTERION obrt za dizajn pruža usluge izrade realističnih 3D vizualizacija interijera i eksterijera te raznih proizvoda.",
+    },
+    "hr"
+  )
+  translate.add(
+    {
+      metaTitle: "ARTERION | 3D interior, exterior and product visualization",
+      metaDesc:
+        "ARTERION offers 3D visualization services, ranging from high-quality interior and exterior renders to 3D product modeling and rendering.",
+    },
+    "en"
+  )
 
-
-  
   return (
-    <main>
-      <Helmet>
-        <title>{translate('metaTitle', null, {locale: locale})}</title>
-        <meta name="description" content={translate('metaDesc', null, {locale: locale})} />
-        <meta name="google-site-verification" content="ErRyGpuFwE1Gita3NspBPxJes2cGWcl8swkuUNB3-00" />
-      </Helmet>
+    <ParallaxProvider>
+      <main>
+        <Helmet>
+          <title>{translate("metaTitle", null, { locale: locale })}</title>
+          <meta
+            name="description"
+            content={translate("metaDesc", null, { locale: locale })}
+          />
+          <meta
+            name="google-site-verification"
+            content="ErRyGpuFwE1Gita3NspBPxJes2cGWcl8swkuUNB3-00"
+          />
+        </Helmet>
 
-      <div>
-        <Header />
-        <Landing />
-        <Gallery />
-        <Contact />
-      </div>
-    </main>
+        <div>
+          <Header />
+          <Landing />
+          <Gallery />
+          <Contact />
+        </div>
+      </main>
+    </ParallaxProvider>
   )
 }
 
